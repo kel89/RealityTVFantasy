d45a559ca4ec694d3ccd088ef4ac0c48d5f2679c
kel89 <kel89@cornell.edu>
2020-01-19 11:34:24 -0500

    start of ingest

---
+++ Helpers/starDisplay.js
@@ -0,0 +1,13 @@
+// starDisplay.js
+
+/*
+Functions that help display all the stars
+*/
+
+function buildStartDisplay(){
+	/*
+	Biilds the start display and 
+	places it in starsDisplay div
+	*/
+	
+}
