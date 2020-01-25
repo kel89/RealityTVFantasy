6b7cd96d8f30b5d612ad728ac7d526bb9b0a5ccb
kel89 <kel89@cornell.edu>
2020-01-25 10:17:24 -0500

    icons on leaderboard

--- Helpers/leaderboardFunctions.js
+++ Helpers/leaderboardFunctions.js
@@ -16,29 +16,24 @@ function buildLeaderboard(){
 	$("#displayArea").html(baseHtml);
 
 	// Setup the data
-	let users = d3.set(data.map(x => x.PickedBy)).values()
-	let scoreData = [];
-	users.forEach(u => scoreData.push({team:u, score:getScore(u)}));
-
-	// Filter out the null and sort
-	scoreData = scoreData.filter(x => x.team != "null")
-	scoreData.sort((a,b) => b.score - a.score);
+	let users = d3.set(data.map(x => x.PickedBy)).values().filter(x => x != "null");
+	users.sort(function(a,b){
+		a = getScore(a);
+		b = getScore(b);
+		return b - a;
+	})
 
 	// Make the table
 	let html = `
 		<table class='leaderboard'>
 			<tr>
+				<th>Player</th>
 				<th>Team</th>
 				<th>Score</th>
 			</tr>
 	`;
-	scoreData.forEach(function(d){
-		html += `
-			<tr onclick='buildUserPage("${d.team}")'>
-				<td>${d.team}</td>
-				<td>${d.score}</td>
-			</tr>
-		`;
+	users.forEach(function(d){
+		html += makeRow(d);
 	});
 	html += "</table>"
 
@@ -46,6 +41,65 @@ function buildLeaderboard(){
 	$("#leaderboard").html(html);
 }
 
+function makeRow(user){
+	/*
+	Takes in a piece of data on a player and returns the html
+	for their leaderboard row
+	This includes their name, their team, their sore, and whatever
+	else we decide to include in the future
+	*/
+	// get the users Data
+	let score = getScore(user);
+
+	// Make team icons
+	let team = data.filter(x => x.PickedBy == user).sort(function(a,b){
+		// sort by show then eliminated
+		let showA = a.Show;
+		let showB = b.Show;
+		let statusA = a.Eliminated;
+		let statusB = b.Eliminated;
+
+		if (showA == showB){
+			return statusA - statusB;
+		}
+		else{
+			return showA > showB ? -1 : 1;
+		}
+	});
+	let starIcons = '<span>';
+	team.forEach(d => starIcons += makeStarIcon(d));
+	starIcons += '</span>'
+
+	let html = `
+		<tr onclick='buildUserPage("${user}")'>
+			<td style='padding-left:5px;'>${user}</td>
+			<td>${starIcons}</td>
+			<td>${score}</td>
+		</tr>
+	`;
+
+	return html;
+}
+
+function makeStarIcon(d){
+	/*
+	Takes in a piece of data containing the star, show, and Eliminated
+	status and returns the html for a starIcon div to be used in the table
+	*/
+	let show = d.Show;
+	let elim = d.Eliminated;
+	let name = d.Name;
+	let html = `
+		<div class='starIcon'
+			style='
+				background: ${showColorDict[show]};
+				opacity: ${elim == 1 ? .25 : 1};
+			'>${abreviationDict[show]}
+		</div>
+
+	`;
+	return html;
+}
 
 function getScore(user){
 	/*
